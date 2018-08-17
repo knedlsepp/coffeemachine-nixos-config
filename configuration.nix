fb59707fad63cccee6f48303a62fee680de94a7d
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-08-17 10:04:18 +0000

    Enable wireless drivers

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,23 @@
   systemd.services.sshd.wantedBy = lib.mkOverride 0 ["multi-user.target"];
 
   nix.checkConfig = false;
-  networking.wireless.enable = lib.mkForce false;
+  networking.wireless.enable = true;
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.firmware = [
+    (pkgs.stdenv.mkDerivation {
+     name = "broadcom-rpi3-extra";
+     src = pkgs.fetchurl {
+     url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/54bab3d/brcm80211/brcm/brcmfmac43430-sdio.txt";
+     sha256 = "19bmdd7w0xzybfassn7x4rb30l70vynnw3c80nlapna2k57xwbw7";
+     };
+     phases = [ "installPhase" ];
+     installPhase = ''
+     mkdir -p $out/lib/firmware/brcm
+     cp $src $out/lib/firmware/brcm/brcmfmac43430-sdio.txt
+     '';
+     })
+  ];
 
   imports = [./sd-image-aarch64.nix];
 
